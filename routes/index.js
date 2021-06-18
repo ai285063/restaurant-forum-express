83f23d337640cdb4744465b1f824dfacf03105ef
Meg <ai285063@gmail.com>
2021-06-18 09:38:27 +0800

    Add admin users index page & allow change user's isAdmin

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,9 @@ module.exports = (app, passport) => {
   app.get('/logout', userController.logout)
 
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
+  app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin)
+
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)

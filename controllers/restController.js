a419a4c2383de5a3f48a88ab0eff544e7a591d79
Meg <ai285063@gmail.com>
2021-06-27 10:20:03 +0800

    Add get top 10 Restaurant Feature

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,6 +4,7 @@ const Category = db.Category
 const Comment = db.Comment
 const User = db.User
 const helpers = require('../_helpers')
+const Sequelize = require('sequelize')
 
 const pageLimit = 10
 
@@ -106,6 +107,57 @@ const restController = {
       ]
     }).then(restaurant => {
       return res.render('dashboard', { restaurant: restaurant.toJSON() })
+    })
+  },
+  getTop10: async (req, res) => {
+    // try {
+    //   const data = await Restaurant.findAll({
+    //     group: ['id'],
+    //     includeIgnoreAttributes: false,
+    //     include: [
+    //       {
+    //         model: User,
+    //         as: 'FavoritedUsers',
+    //         duplicating: false
+    //       }
+    //     ],
+    //     limit: 10,
+    //     attributes: [
+    //       'id', 'name', 'description', 'image',
+    // 這行 Sequelize.fn 這樣寫的話會讓沒有人收藏的餐廳明明是 FavoritedUsers:[]，但會被算成 FavoritedCount 有 1 因為有 1 個 array
+    //       [Sequelize.fn('COUNT', 'FavoritedUsers'), 'FavoritedCount']
+    //     ],
+    //     order: [[Sequelize.literal('FavoritedCount'), 'DESC'], ['id']]
+    //   })
+    //   const top10Rest = data.map(restaurant => ({
+    //     ...restaurant.dataValues,
+    //     isFavorited: helpers.getUser(req).FavoritedUsers.map(d => d.id).includes(restaurant.id)
+    //   }))
+    //     return res.render('top10Rest', { restaurants: top10Rest })
+    // } catch (error) {
+    //   req.flash('error_messages', error.toString())
+    //   return res.redirect('back')
+    // }
+
+    return Restaurant.findAll({
+      include: [
+        {
+          model: User,
+          as: 'FavoritedUsers'
+        }
+      ]
+    }).then(restaurants => {
+      restaurants = restaurants.map(restaurant => ({
+        ...restaurant.dataValues,
+        FavoritedCount: restaurant.FavoritedUsers.length,
+        isFavorited: restaurant.FavoritedUsers.map(d => d.id).includes(helpers.getUser(req).id)
+      }))
+      restaurants.sort((a, b) => b.FavoritedCount - a.FavoritedCount)
+      restaurants = restaurants.slice(0, 10)
+
+      return res.render('top10Rest', {
+        restaurants: restaurants
+      })
     })
   }
 }

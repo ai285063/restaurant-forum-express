4903a129341c9cc42b3e932dea161ebf480f5a34
Meg <ai285063@gmail.com>
2021-06-29 13:19:16 +0800

    POST /api/admin/restaurants

--- services/adminService.js
+++ services/adminService.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminService = {
   getRestaurants: (req, res, callback) => {
@@ -25,6 +27,43 @@ const adminService = {
             callback({ status: 'success', message: '' })
           })
       })
+  },
+  postRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" })
+    }
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        if (err) {
+          callback({ status: 'error', message: 'image upload error' })
+        }
+        return Restaurant.create({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description,
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
+        }).then((restaurant) => {
+          callback({ status: 'success', message: 'restaurant was successfully created' })
+        })
+      })
+    } else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        CategoryId: req.body.categoryId
+      })
+        .then((restaurant) => {
+          callback({ status: 'success', message: 'restaurant was successfully created' })
+        })
+    }
   }
 }
 module.exports = adminService
